9d7c1d2c387733746a4a09fa885b80355030866f
Beatriz Trindade <beatriztrindade@hotmail.com>
2022-08-18 10:30:19 -0300

    @BeaTrindade [task: Delete IAutenticacao de Repositorio] {18/08/2022}

--- BlogAPI/Startup.cs
+++ BlogAPI/Startup.cs
@@ -1,18 +1,17 @@
 using BlogAPI.Src.Contextos;
 using BlogAPI.Src.Repositorios;
 using BlogAPI.Src.Repositorios.Implementacoes;
+using BlogAPI.Src.Servicos;
+using BlogAPI.Src.Servicos.Implementacoes;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace BlogAPI
 {
@@ -39,6 +38,28 @@ namespace BlogAPI
             // Controladores
             services.AddCors();
             services.AddControllers();
+
+            // Configuração de serviços 
+            services.AddScoped<IAutenticacao, AutenticacaoServicos>();
+
+            // Configuração do Token Autenticação JWTBearer
+            var chave = Encoding.ASCII.GetBytes(Configuration["Settings:Secret"]);
+            services.AddAuthentication(a =>
+            {
+                a.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                a.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(b =>
+            {
+                b.RequireHttpsMetadata = false;
+                b.SaveToken = true;
+                b.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(chave),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,7 +72,7 @@ namespace BlogAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            // Ambiente de produção
+            // Ambiente de producao
             contexto.Database.EnsureCreated();
 
             app.UseRouting();
@@ -62,6 +83,8 @@ namespace BlogAPI
                 .AllowAnyHeader()
                 );
 
+            // Autenticação e Autorizacao
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
